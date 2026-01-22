da48f8880840955be9aff560e7939d9a385bb63f
Bob <bob@justbetter.nl>
2026-01-22 09:57:35 +0100

    Init scaffolding

---
+++ tests/TestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JustBetter\Veto\Tests;
+
+use JustBetter\Veto\ServiceProvider;
+use Statamic\Testing\AddonTestCase;
+use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
+
+class TestCase extends AddonTestCase
+{
+    use PreventsSavingStacheItemsToDisk;
+
+    protected string $addonServiceProvider = ServiceProvider::class;
+
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $app['config']->set('statamic.editions.pro', true);
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('app.key', 'base64:7tG0yY7g3QkFrQ+Vk4EBSbcT8D9C4/5Dph1dNRjh6WU=');
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
